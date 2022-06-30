09662f7f55193708c3c1fe691d9ce639c197c3be
jyeonki <kay8825@naver.com>
2022-06-30 15:48:05 +0900

    [220630] jdbc Score App

---
+++ src/test/java/com/jdbc/basic/score/domain/ScoreTest.java
@@ -0,0 +1,44 @@
+package com.jdbc.basic.score.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreTest {
+
+    @Test
+    void lombokTest() {
+        Score s = new Score(1, "김철수", 99, 88, 11, 35, 60);
+        s.setTotal(222);
+
+        System.out.println(s.getStuName());
+        assertEquals(s.getStuName(), "김철수");
+
+    }
+
+
+    @Test
+    void lombokBuilderTest() {
+        Score park = new Score.ScoreBuilder()
+                            .stuNum(2)
+                            .stuName("박영희")
+                            .math(92)
+                            .eng(100)
+                            .total(192)
+                            .average(96).build();
+
+        System.out.println(park);
+    }
+}
+
+
+
+
+
+
+
+
+
+
